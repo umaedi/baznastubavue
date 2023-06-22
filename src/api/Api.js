60725ff365af4139d757f71f36343fa3275b675b
= <=>
2023-06-22 11:11:47 +0700

    baznastuba vue

---
+++ src/api/Api.js
@@ -0,0 +1,10 @@
+//import axios
+import axios from 'axios'
+
+const Api = axios.create({
+    //set default endpoint API
+    // baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://baznastuba.tulangbawangkab.go.id/api'
+})
+
+export default Api
